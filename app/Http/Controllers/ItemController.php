095fad14522e4556f760e6796deb1b0cf8c41e9a
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-09-20 00:06:32 +0900

    index.bladeとshow.blade作成完了

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -7,6 +7,15 @@ use App\Models\Item;
 
 class ItemController extends Controller
 {
+    public function index()
+    {
+        // モデル名::テーブル全件取得
+        $items = Item::all();
+        $data = ['items' => $items];
+        // Itemディレクトリの中のindexページを指定しitemsの連想配列を代入
+        return view('items.index', $data);
+    }
+
     public function show($id)
     {
         $item = Item::find($id);
--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -7,5 +7,18 @@
     <title>Planet-App</title>
 </head>
 <body>
+    <h1>惑星情報</h1>
+    @if ($item !== null)
+    <div class="item-field">
+        <strong>名前 : {{ $item->name }}</strong>
+        <strong>名前(英語) : {{ $item->englishname }}</strong>
+        <strong>半径 : {{ $item->radius }}</strong>
+        <strong>重量 : {{ $item->weight }}</strong>
+    </div>
+
+    @else
+    <p>id = {{ $id }} の惑星情報は存在しません。</p>
+    @endif
+    <a href="/items">戻る</a>
 </body>
 </html>
